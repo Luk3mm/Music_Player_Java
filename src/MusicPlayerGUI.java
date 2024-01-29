aa1b5646977ec44adcc108a13f4438d8c221a983
Lucas Mendes <lucas39lm@hotmail.com>
2024-01-29 07:38:39 -0300

    Music Player

---
+++ src/MusicPlayerGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MusicPlayerGUI extends JFrame {
+    public MusicPlayerGUI(){
+        super("Luke Player");
+
+        setSize(400, 600);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        setResizable(false);
+
+        setLayout(null);
+    }
+}
